35cc099e51855582e3434591eaa544809211d05e
migueelmartinezzz <maartinez.miguel476@gmail.com>
2024-11-23 15:08:12 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,1 +1,4 @@
+st.title("Prospectec")
+st.subheader("Encontre parceiros e conheça seus concorrentes")
+st.markdown("Utilize nossa ferramenta para identificar empresas semelhantes em poucos cliques!")
 
