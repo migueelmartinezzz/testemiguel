a973cc0b7d8d70417f30a0cc8aabd788a78eb203
migueelmartinezzz <maartinez.miguel476@gmail.com>
2024-11-23 15:32:17 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,7 +1,31 @@
 import streamlit as st
 import pandas as pd
-st.title("Prospectec")
-st.subheader("Encontre parceiros e conheça seus concorrentes")
+st.markdown(
+    """
+    <style>
+    @import url('https://fonts.google.com/specimen/Fjalla+One?preview.text=Prospectec');
+    .titulo {
+        font-size: 36px;
+        color: #003366; /* Azul escuro */
+        font-family: 'Poppins', sans-serif; /* Fonte personalizada */
+        text-align: center; /* Centralizado */
+    }
+    .subtitulo {
+        font-size: 24px;
+        font-weight: bold; /* Negrito */
+        color: #333333; /* Cinza escuro */
+        font-family: 'Poppins', sans-serif;
+        text-align: center; /* Centralizado */
+        margin-top: -10px; /* Ajuste opcional para aproximar o subtítulo */
+    }
+    </style>
+    <h1 class="titulo">Prospectec</h1>
+    <h2 class="subtitulo">Encontre parceiros e conheça seus concorrentes</h2>
+    """,
+    unsafe_allow_html=True
+)
+
+# Descrição adicional
 st.markdown("Utilize nossa ferramenta para identificar empresas semelhantes em poucos cliques!")
 
 
